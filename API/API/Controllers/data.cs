76a6bdafd5e28dea453dfa5f4dc5724e748ef313
Jomaro96 <56599883+Jomaro96@users.noreply.github.com>
2024-06-09 23:50:00 -0500

    Added the functional API

---
+++ API/API/Controllers/data.cs
@@ -0,0 +1,29 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Controlador que enlista todos mis objectos del archivo json en base a mi modelo de datos y da una respuesta.
+    /// Liga de la API para su consumo en frontend/postman: http://localhost:5293/api/data
+    /// </summary>
+    [ApiController]
+    [Route("api/data")]
+    public class DataController:ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Data>> Get()
+        {
+            try
+            {
+                var dataList = json_handler.ReadJson();
+                return Ok(dataList);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message, detail = ex.InnerException?.Message });
+            }
+            
+        }
+    }
+}
---
+++ API/API/json_handler.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text.Json;
+using API.Models;
+namespace API
+{
+    /// <summary>
+    /// clase que se encarga de la deserializacion del archivo json
+    /// </summary>
+    public class json_handler
+    {
+        private static readonly string fileJson = "data.json";
+        public static List<Data> ReadJson()
+        {
+            try
+            {
+                if (!File.Exists(fileJson))
+                {
+                    throw new FileNotFoundException("No se encontro el archivo JSON.");
+                }
+                var jsonData = File.ReadAllText(fileJson);
+                var dataList = JsonSerializer.Deserialize<List<Data>>(jsonData);
+
+                return dataList ?? new List<Data>();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error intentando leer el archivo JSON", e);
+               
+            }
+            
+        }
+    }
+}
